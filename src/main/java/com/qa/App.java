5275b4ae966f3273b8e12d30b6b1aeb8c0c47c06
james-richards <james@jamesrrichards.com>
2026-06-10 10:04:37 +0000

    NEW: 2.0 release

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("James"));
         System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println("This is version 2.0");
         System.out.println(sayGoodbye());
     }
 
